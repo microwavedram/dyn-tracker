e804cdd5adc016feacfbf35118ba9ea8be872d35
microwavedram <89029844+microwavedram@users.noreply.github.com>
2023-03-27 19:44:44 +0100

    new changes

--- dist/index.js
+++ dist/index.js
@@ -34,11 +34,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = __importStar(require("dotenv"));
 const fsd = __importStar(require("fs"));
+const discord_js_1 = require("discord.js");
 dotenv.config();
+const COOLDOWN = 10000;
 const DYNMAP_URI = "http://globecraft.eu:8033";
 const WORLD_FILE = "world";
 const DATABASE_URL = "https://raw.githubusercontent.com/microwavedram/dyn-tracker/master/database.json";
+const LOG_CHANNEL_ID = "1088874515209146429";
+const GUILD_ID = "1085648041354199170";
 const writeStream = fsd.createWriteStream("./session.csv", { encoding: "utf-8" });
+const discord_client = new discord_js_1.Client({ intents: [discord_js_1.IntentsBitField.Flags.Guilds, discord_js_1.IntentsBitField.Flags.GuildMessages] });
+let log_cache = new Map();
+let zone_cache = new Map();
+let session_mutes = [];
 let Database;
 const BYPASS = [
     "agnat",
@@ -83,8 +91,20 @@ function getInfoForDimention(dim) {
 }
 function checkPositions(players) {
     return __awaiter(this, void 0, void 0, function* () {
+        zone_cache.forEach((value, key) => {
+            if (value) {
+                const username = key.split(":")[1];
+                if (!players.some(player => player.account == username)) {
+                    // offline
+                    log_cache.delete(key);
+                    zone_cache.delete(key);
+                }
+            }
+        });
         for (let i = 0; i < players.length; i++) {
             const player = players[i];
+            if (player.world != "world")
+                continue;
             if (BYPASS.includes(player.account))
                 continue;
             for (let i = 0; i < Database.watched_locations.length; i++) {
@@ -96,6 +116,7 @@ function checkPositions(players) {
                 const dx = location.coords[0] - player.x;
                 const dz = location.coords[1] - player.z;
                 const distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dz, 2));
+                // if (distance <= 2000) {
                 if (distance <= location.radius) {
                     const cooldown_time = location_cooldowns === null || location_cooldowns === void 0 ? void 0 : location_cooldowns.get(player.account);
                     if (cooldown_time) {
@@ -110,19 +131,29 @@ function checkPositions(players) {
                     //console.log(allowed_teams.map(team => `${team.members.map(member => member.username).join()}`).join())
                     if (allowed_teams.some(team => team.members.some(member => member.username == player.account)))
                         continue;
-                    location_cooldowns === null || location_cooldowns === void 0 ? void 0 : location_cooldowns.set(player.account, Date.now() + 30000);
+                    if (session_mutes.includes(`${location.name}:${player.name}`))
+                        continue;
+                    location_cooldowns === null || location_cooldowns === void 0 ? void 0 : location_cooldowns.set(player.account, Date.now() + COOLDOWN);
                     console.log(`PLAYER TRESSPASSING [${location.teams.join()}'s ${location.name}] : ${player.account} : ${player.x}, ${player.y}, ${player.z} [${distance} blocks from center]`);
                     //@ts-ignore
-                    const res = yield fetch(process.env.WEBHOOK, {
-                        "method": "POST",
-                        "body": yield JSON.stringify({
-                            content: `<@&1089254990499029014> PLAYER TRESSPASSING [${location.teams.map(teamname => teamname + "'s").join(", ")} ${location.name}] : ${player.account} : ${player.x}, ${player.y}, ${player.z} [${distance} blocks from center]`,
-                        }),
-                        "headers": {
-                            "Content-Type": "application/json"
-                        }
-                    });
+                    zone_cache.set(`${location.name}:${player.name}`, true);
+                    yield createLogMessage(player, location);
+                    // const res = await fetch(process.env.WEBHOOK, {
+                    //     "method": "POST",
+                    //     "body": await JSON.stringify({
+                    //         content: `<@&1089254990499029014> PLAYER TRESSPASSING [${location.teams.map(teamname => teamname + "'s").join(", ")} ${location.name}] : ${player.account} : ${player.x}, ${player.y}, ${player.z}`,
+                    //     }),
+                    //     "headers": {
+                    //         "Content-Type": "application/json"
+                    //     }
+                    // })
                     //console.log(await res.text())
+                }
+                else {
+                    const removed = zone_cache.delete(`${location.name}:${player.name}`);
+                    if (removed) {
+                        log_cache.delete(`${location.name}:${player.name}`);
+                    }
                 }
                 if (location_cooldowns) {
                     cooldowns.set(location.name, location_cooldowns);
@@ -138,23 +169,152 @@ function logPlayers(players) {
         });
     });
 }
+function createLogMessage(player, location) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const guild = discord_client.guilds.cache.get(GUILD_ID);
+        if (!guild)
+            return;
+        const channel = guild.channels.cache.get(LOG_CHANNEL_ID);
+        const log = log_cache.get(`${location.name}:${player.name}`);
+        let color = 0xffffff;
+        let next_ignore = false;
+        if (log && log.mute_updated)
+            return;
+        if (log && log.muted) {
+            color = 0xff0000;
+            next_ignore = true;
+            return;
+        }
+        ;
+        const dx = location.coords[0] - player.x;
+        const dz = location.coords[1] - player.z;
+        const distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dz, 2));
+        let bearing = (Math.atan2(-dx, dz) * (180 / Math.PI)) % 360;
+        if (bearing < 0)
+            bearing += 360;
+        // kill me
+        let dir = "";
+        if (bearing > 360 - (45 / 2) || bearing <= (45 / 2))
+            dir = "N";
+        if (bearing > (45 / 2) && bearing <= 90 - (45 / 2))
+            dir = "NE";
+        if (bearing > 90 - (45 / 2) && bearing <= 90 + (45 / 2))
+            dir = "E";
+        if (bearing > 90 + (45 / 2) && bearing <= 180 - (45 / 2))
+            dir = "SE";
+        if (bearing > 180 - (45 / 2) && bearing <= 180 + (45 / 2))
+            dir = "S";
+        if (bearing > 180 + (45 / 2) && bearing <= 270 - (45 / 2))
+            dir = "SW";
+        if (bearing > 270 - (45 / 2) && bearing <= 270 + (45 / 2))
+            dir = "W";
+        if (bearing > 270 + (45 / 2) && bearing <= 360 - (45 / 2))
+            dir = "NW";
+        let detections = 1;
+        if (log) {
+            detections = log.detections + 1;
+        }
+        const embed = new discord_js_1.EmbedBuilder();
+        embed.setTitle(`Tresspass log ${player.name}, ${location.name}`);
+        embed.setDescription(`${player.name} [${player.account}] was detected within ${location.name}`);
+        embed.addFields([
+            { name: "Distance", value: `${distance}`, "inline": true },
+            { name: "Bearing", value: `[${dir}] ${bearing} degrees`, "inline": true },
+            { name: "Detections", value: `${detections}`, "inline": true }
+        ]);
+        embed.setColor(color);
+        embed.setTimestamp();
+        embed.setAuthor({
+            name: "Azorix Satellite Monitoring"
+        });
+        const row = new discord_js_1.ActionRowBuilder()
+            .addComponents(new discord_js_1.ButtonBuilder()
+            .setLabel("Mute This Instance [until they leave]")
+            .setEmoji("🔉")
+            .setStyle(discord_js_1.ButtonStyle.Primary)
+            .setCustomId("mute-instance"), new discord_js_1.ButtonBuilder()
+            .setLabel("Mute For Session [until bot crash]")
+            .setEmoji("🔇")
+            .setStyle(discord_js_1.ButtonStyle.Danger)
+            .setCustomId("mute-session"));
+        const msg = {
+            content: `<@&1089254990499029014> PLAYER TRESSPASSING [${location.teams.map(teamname => teamname + "'s").join(" and ")} ${location.name}] : ${player.account}`,
+            embeds: [embed],
+            components: [row]
+        };
+        if (log) {
+            const message = channel.messages.cache.get(log.message_id);
+            log.detections = detections;
+            if (next_ignore)
+                log.mute_updated = true;
+            log_cache.set(`${location.name}:${player.name}`, log);
+            if (message) {
+                yield message.edit(msg);
+                return;
+            }
+        }
+        const message = yield channel.send(msg);
+        log_cache.set(`${location.name}:${player.name}`, {
+            message_id: `${message.id}`,
+            timestamp: Date.now(),
+            muted: false,
+            detections: 1,
+            mute_updated: false,
+            player: player,
+            location: location
+        });
+    });
+}
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        console.log("Started up the satellite");
-        while (true) {
-            const data = yield getInfoForDimention("world");
-            if (data) {
-                yield refetch_database();
-                //@ts-ignore
-                yield logPlayers(data.players);
-                //@ts-ignore
-                yield checkPositions(data.players);
+        console.log("Initialising satellite.");
+        discord_client.on("interactionCreate", (interaction) => __awaiter(this, void 0, void 0, function* () {
+            if (interaction.isButton()) {
+                const message = interaction.message;
+                switch (interaction.customId) {
+                    case "mute-instance":
+                        log_cache.forEach((value, key) => {
+                            if (value.message_id == message.id) {
+                                value.muted = true;
+                                log_cache.set(key, value);
+                                console.log(log_cache);
+                            }
+                        });
+                        interaction.reply({ content: "Muted.", ephemeral: true });
+                        break;
+                    case "mute-session":
+                        interaction.reply({ content: "Muted", ephemeral: true });
+                        log_cache.forEach((value, key) => {
+                            if (value.message_id == message.id) {
+                                value.muted = true;
+                                log_cache.set(key, value);
+                                session_mutes.push(`${value.location.name}:${value.player.name}`);
+                            }
+                        });
+                        break;
+                }
             }
-            else {
-                console.log("server probably down");
+        }));
+        discord_client.on("ready", () => __awaiter(this, void 0, void 0, function* () {
+            var _a, _b;
+            console.log(`Discord Bot Running (${(_a = discord_client.user) === null || _a === void 0 ? void 0 : _a.username}#${(_b = discord_client.user) === null || _b === void 0 ? void 0 : _b.discriminator})!`);
+            console.log("Started up the satellite");
+            while (true) {
+                const data = yield getInfoForDimention("world");
+                if (data) {
+                    yield refetch_database();
+                    //@ts-ignore
+                    yield logPlayers(data.players);
+                    //@ts-ignore
+                    yield checkPositions(data.players);
+                }
+                else {
+                    console.log("server probably down");
+                }
+                yield sleep(2000);
             }
-            yield sleep(2000);
-        }
+        }));
+        discord_client.login(process.env.DISCORD_TOKEN);
     });
 }
 process.removeAllListeners("warning"); // FUCK YOU FETCH WARNINGS
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 import * as dotenv from "dotenv" 
 import * as fs from "fs/promises"
 import * as fsd from "fs"
-import { ActionRow, ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, EmbedBuilder, Guild, IntentsBitField, Message, MessageCreateOptions, MessageEditOptions, MessagePayload, TextChannel } from "discord.js"
-import { MessageOptions } from "child_process"
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, EmbedBuilder, Guild, IntentsBitField, Message,  MessageEditOptions, TextChannel } from "discord.js"
 
 dotenv.config()
 
+const COOLDOWN = 10000
 const DYNMAP_URI = "http://globecraft.eu:8033"
 const WORLD_FILE = "world"
 const DATABASE_URL = "https://raw.githubusercontent.com/microwavedram/dyn-tracker/master/database.json"
-const LOG_CHANNEL_ID = "0"
-const GUILD_ID = "0"
+const LOG_CHANNEL_ID = "1088874515209146429"
+const GUILD_ID = "1085648041354199170"
 
 const writeStream = fsd.createWriteStream("./session.csv", { encoding: "utf-8" })
 
 const discord_client = new Client({ intents: [ IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages ] })
 
 let log_cache: Map<string, Log> = new Map();
+let zone_cache: Map<string, boolean> = new Map();
+let session_mutes: string[] = []
 
 let Database: Database
 
@@ -39,8 +41,12 @@ interface Database {
 interface Log {
     message_id: string,
     timestamp: number,
-    disabled: boolean,
-    unmute_at: number
+    muted: boolean,
+    detections: number,
+    mute_updated: boolean
+
+    player: Player,
+    location: WorldLocation
 }
 
 interface Player {
@@ -76,7 +82,8 @@ interface WorldLocation {
     coords: number[],
     radius: number,
 
-    teams: string[]
+    teams: string[],
+    isPlayer?: boolean
 }
 
 const getTeamFromTeamName = (teamname:string) => Database.teams.find(team => team.name == teamname) || {"name":"undefined", "members": [], "vassals": []}
@@ -117,9 +124,21 @@ async function getInfoForDimention(dim: string) {
 
 async function checkPositions(players: Player[]) {
 
+    zone_cache.forEach((value, key) => {
+        if (value) {
+            const username = key.split(":")[1]
+            if (!players.some(player => player.account == username)) {
+                // offline
+                log_cache.delete(key)
+                zone_cache.delete(key)
+            }
+        }
+    })
+
     for (let i = 0; i < players.length; i++) {
         const player = players[i];
-        
+
+        if (player.world != "world") continue
         if (BYPASS.includes(player.account)) continue;
 
         for (let i = 0; i < Database.watched_locations.length; i++) {
@@ -135,6 +154,7 @@ async function checkPositions(players: Player[]) {
             
             const distance = Math.sqrt( Math.pow(dx,2) + Math.pow(dz,2) )
             
+            // if (distance <= 2000) {
             if (distance <= location.radius) {
                 const cooldown_time = location_cooldowns?.get(player.account)
                 if (cooldown_time) {
@@ -153,12 +173,14 @@ async function checkPositions(players: Player[]) {
 
                 if (allowed_teams.some(team => team.members.some(member => member.username == player.account))) continue
 
-                location_cooldowns?.set(player.account, Date.now() + 30000)
+                if (session_mutes.includes(`${location.name}:${player.name}`)) continue
+
+                location_cooldowns?.set(player.account, Date.now() + COOLDOWN)
                 console.log(`PLAYER TRESSPASSING [${location.teams.join()}'s ${location.name}] : ${player.account} : ${player.x}, ${player.y}, ${player.z} [${distance} blocks from center]`)
 
                 //@ts-ignore
-
-                await createLogMessage(player, location, distance)
+                zone_cache.set(`${location.name}:${player.name}`, true)
+                await createLogMessage(player, location)
 
                 // const res = await fetch(process.env.WEBHOOK, {
                 //     "method": "POST",
@@ -171,6 +193,11 @@ async function checkPositions(players: Player[]) {
                 // })
                 
                 //console.log(await res.text())
+            } else {
+                const removed = zone_cache.delete(`${location.name}:${player.name}`)
+                if (removed) {
+                    log_cache.delete(`${location.name}:${player.name}`)
+                }
             }
 
             if (location_cooldowns) {
@@ -186,82 +213,169 @@ async function logPlayers(players: Player[]) {
     })
 }
 
-async function createLogMessage(player: Player, location: WorldLocation, distance: number) {
+async function createLogMessage(player: Player, location: WorldLocation) {
     const guild: Guild | undefined = discord_client.guilds.cache.get(GUILD_ID)
     if (!guild) return;
     const channel = guild.channels.cache.get(LOG_CHANNEL_ID) as TextChannel
+
+    const log = log_cache.get(`${location.name}:${player.name}`)
+
+    let color = 0xffffff
+    let next_ignore = false
+
+    if (log && log.mute_updated) return;
+    if (log && log.muted) { color = 0xff0000; next_ignore = true; return };
+
+    const dx = location.coords[0] - player.x
+    const dz = location.coords[1] - player.z
     
+    const distance = Math.sqrt( Math.pow(dx,2) + Math.pow(dz,2) )
+    let bearing = (Math.atan2(-dx,dz) * (180/Math.PI)) % 360
+
+    if (bearing < 0) bearing += 360;
+
+    // kill me
+    let dir = ""
+    if (bearing > 360-(45/2) || bearing <= (45/2)) dir = "N";
+    if (bearing > (45/2) && bearing <= 90-(45/2)) dir = "NE";
+    if (bearing > 90-(45/2) && bearing <= 90+(45/2)) dir = "E";
+    if (bearing > 90+(45/2) && bearing <= 180-(45/2)) dir = "SE";
+    if (bearing > 180-(45/2) && bearing <= 180+(45/2)) dir = "S";
+    if (bearing > 180+(45/2) && bearing <= 270-(45/2)) dir = "SW";
+    if (bearing > 270-(45/2) && bearing <= 270+(45/2)) dir = "W";
+    if (bearing > 270+(45/2) && bearing <= 360-(45/2)) dir = "NW";
+    
+    let detections = 1
+    if (log) {
+        detections = log.detections + 1
+    }
+
     const embed = new EmbedBuilder()
     embed.setTitle(`Tresspass log ${player.name}, ${location.name}`)
+    embed.setDescription(`${player.name} [${player.account}] was detected within ${location.name}`)
+    embed.addFields([
+        {name: "Distance", value: `${distance}`, "inline": true},
+        {name: "Bearing", value: `[${dir}] ${bearing} degrees`, "inline": true},
+        {name: "Detections", value: `${detections}`, "inline": true}
+    ])
+    embed.setColor(color)
+    embed.setTimestamp()
+    embed.setAuthor({
+        name: "Azorix Satellite Monitoring"
+    })
 
     const row = new ActionRowBuilder()
     .addComponents(
         new ButtonBuilder()
-        .setLabel("Mute Instance [until leave]")
+        .setLabel("Mute This Instance [until they leave]")
         .setEmoji("🔉")
         .setStyle(ButtonStyle.Primary)
         .setCustomId("mute-instance"),
         new ButtonBuilder()
-        .setLabel("Mute Session [until bot crash]")
+        .setLabel("Mute For Session [until bot crash]")
         .setEmoji("🔇")
         .setStyle(ButtonStyle.Danger)
         .setCustomId("mute-session"),
     )
 
-    const msg: MessageCreateOptions = {
+    const msg: any = {
         content: `<@&1089254990499029014> PLAYER TRESSPASSING [${location.teams.map(teamname => teamname + "'s").join(" and ")} ${location.name}] : ${player.account}`,
         embeds : [embed],
         components: [row]
     }
 
-    const log = log_cache.get(`${location.name}:${player.name}`)
     if (log) {
         const message: Message<true> | undefined = channel.messages.cache.get(log.message_id)
 
+        log.detections = detections
+        if (next_ignore) log.mute_updated = true;
+        log_cache.set(`${location.name}:${player.name}`, log)
+
+        
         if (message) {
             await message.edit(msg as MessageEditOptions)
-
+            
             return;
         }
 
     }
     const message = await channel.send(msg)
 
-    
+    log_cache.set(`${location.name}:${player.name}`, {
+        message_id: `${message.id}`,
+        timestamp: Date.now(),
+        muted: false,
+        detections: 1,
+        mute_updated: false,
+
+        player: player,
+        location: location
+    })
 }
 
 async function main() {
 
-    console.log("Initialising discord bot.")
+    console.log("Initialising satellite.")
 
-    discord_client.on("interactionCreate", interaction => {
+    discord_client.on("interactionCreate", async interaction => {
+        if (interaction.isButton()) {
+            const message = interaction.message
 
+            switch (interaction.customId) {
+                case "mute-instance":
+                    log_cache.forEach((value, key) => {
+                        if (value.message_id == message.id) {
+                            value.muted = true
+                            log_cache.set(key, value)
+                            console.log(log_cache)
+                        }
+                    });
+
+                    interaction.reply({content: "Muted.", ephemeral: true})
+
+                    break;
+                case "mute-session":
+                    interaction.reply({content: "Muted", ephemeral: true})
+
+                    log_cache.forEach((value, key) => {
+                        if (value.message_id == message.id) {
+                            value.muted = true
+                            log_cache.set(key, value)
+                            session_mutes.push(`${value.location.name}:${value.player.name}`)
+                        }
+                    });
+
+
+                    break;
+            }
+        }
     })
 
-    discord_client.on("ready", () => {
-        console.log(`Client Ready ${discord_client.user?.username}!`)
+    discord_client.on("ready", async () => {
+        console.log(`Discord Bot Running (${discord_client.user?.username}#${discord_client.user?.discriminator})!`)
+        console.log("Started up the satellite")
+
+        while (true) {
+            const data = await getInfoForDimention("world")
+
+            if (data) {
+                await refetch_database()
+
+                //@ts-ignore
+                await logPlayers(data.players)
+                //@ts-ignore
+                await checkPositions(data.players)
+            } else {
+                console.log("server probably down")
+            }
+
+            await sleep(2000)
+        }
     })
 
     discord_client.login(process.env.DISCORD_TOKEN)
 
-    console.log("Started up the satellite")
-
-    while (true) {
-        const data = await getInfoForDimention("world")
-
-        if (data) {
-            await refetch_database()
-
-            //@ts-ignore
-            await logPlayers(data.players)
-            //@ts-ignore
-            await checkPositions(data.players)
-        } else {
-            console.log("server probably down")
-        }
-
-        await sleep(2000)
-    }
+    
 }
 
 process.removeAllListeners("warning") // FUCK YOU FETCH WARNINGS
